320833a459a3da2ccc7e3c200510c95ceefe7bb6
Jeannie Kim <88344337+jeanniekim@users.noreply.github.com>
2022-02-18 18:08:02 -0800

    Update strings.ts
    let's see if this works

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,9 +1,9 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = ['WOWIE! You\'ve done it now...', 'Amazong.', 'Well did!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found. Suck me.'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
@@ -13,7 +13,7 @@ export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
+export const DELETE_TEXT = 'WAIT GO BACK'
 export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
 export const NEW_WORD_TEXT = 'New word in'
