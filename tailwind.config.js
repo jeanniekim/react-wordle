62322ee88151a9cb2d3b79c093808c6fff529f91
Aaron <10217842+byteduck@users.noreply.github.com>
2022-03-03 21:42:43 -0800

    stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          "500": "#b59f3b"
+        },
+        green: {
+          "500": "#538d4e"
+        },
+        slate: {
+          "100": "#3a3a3c",
+          "600": "#262627",
+          "700": "#3a3a3c",
+          "800": "#3a3a3c",
+          "900": "#121213"
+        }
+      }
+    },
   },
   plugins: [],
 }
